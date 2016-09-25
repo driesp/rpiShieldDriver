fd0c938b12d2a70b2a347ee6ec7a1655681cad32
driesp <dries.peeters@dspautomation.be>
2016-09-25 08:38:00 +0000

    Created a Knightrider led stream. Think i have one broken led.

--- ledTest/main.c
+++ ledTest/main.c
@@ -1,7 +1,12 @@
 #include <bcm2835.h>
 #include <stdio.h>
 
-#define PIN RPI_GPIO_P1_11
+#define ledCount 8
+
+void setOutputs();
+
+RPiGPIOPin leds[ledCount] = {RPI_GPIO_P1_11,RPI_GPIO_P1_12,RPI_GPIO_P1_13,RPI_GPIO_P1_15,RPI_GPIO_P1_16,RPI_GPIO_P1_18,RPI_GPIO_P1_08,RPI_GPIO_P1_07};
+
 
 int main (void)
 {
@@ -9,21 +14,39 @@ int main (void)
 	{
 		return 1;
 	}
-	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+	setOutputs();
 	while (1)
     	{
+	int i;
+	for(i = 0; i < ledCount; i++)
+	{
         	// Turn it on
-        	bcm2835_gpio_write(PIN, HIGH);
-        
+        	bcm2835_gpio_write(leds[i], HIGH);
         	// wait a bit
-        	bcm2835_delay(500);
-        
+        	bcm2835_delay(50);
         	// turn it off
-        	bcm2835_gpio_write(PIN, LOW);
-        
-        	// wait a bit
-        	bcm2835_delay(500);
+        	bcm2835_gpio_write(leds[i], LOW);
+       	}
+	for(i = 7; i >= 0; i--)
+        {
+                // Turn it on
+                bcm2835_gpio_write(leds[i], HIGH);
+                // wait a bit
+                bcm2835_delay(50);
+                // turn it off
+                bcm2835_gpio_write(leds[i], LOW);
+        }
+
     	}
     bcm2835_close();
     return 0;
+}
+
+void setOutputs()
+{
+	int i;
+	for( i = 0; i < ledCount ; i++)
+	{
+		bcm2835_gpio_fsel(leds[i],BCM2835_GPIO_FSEL_OUTP);
+	}
 }
