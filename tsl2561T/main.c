302a2db734ee1516b4a2f1a8da8c569acd250ceb
driesp <dries.peeters@dspautomation.be>
2016-09-29 06:50:51 +0000

    added i2c support for TSL

---
+++ tsl2561T/main.c
@@ -0,0 +1,53 @@
+#include <bcm2835.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+	if(!bcm2835_init())
+	{
+	return 1;
+	}
+	
+
+	char command[1];
+	int val[2];
+	int i = 1;
+
+	bcm2835_i2c_begin();
+	bcm2835_i2c_setSlaveAddress(0x29);
+	bcm2835_i2c_set_baudrate(1000);
+
+	command[0] = 0xa0;
+	bcm2835_i2c_write(command,1);
+	command[0] = 0x03;
+	bcm2835_i2c_write(command,1);
+	bcm2835_delay(500);
+
+	while(i)
+	{
+		command[0] = 0xac;
+		bcm2835_i2c_write(command,1);
+		bcm2835_i2c_read(command,1);
+		val[1] = (int)command;
+		command[0] = 0xad;
+                bcm2835_i2c_write(command,1);
+                bcm2835_i2c_read(command,1);
+                val[0] = (int)command;
+
+		printf("TSL  value:%d\n",val[0]*256+val[1]);
+		bcm2835_delay(100);
+		char inputC;
+		printf("q to quit of r to read again:");
+		scanf("%c%*c", &inputC);
+		switch(inputC)
+		{
+		case 'q': i = 0; break;
+		case 'r': i = 1; break;
+		default: i = 1; break;
+		}
+	}
+	bcm2835_i2c_end();
+	bcm2835_close();
+	return 0;
+	
+}
